0dda2a7cf0054f3d2a3dc0a09fdec55d91c9d934
Luan Bianchini <luanbianchine1@gmail.com>
2022-11-30 14:56:19 -0300

    setup's project done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.send('oi')
+})
+
+mongoose.connect('mongodb://127.0.0.1:27017/sisage')
+
+app.listen(8080, () => {console.log('server running..')})
